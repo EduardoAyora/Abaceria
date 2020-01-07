d37b443b9565b04f9a643f49991e11e9c67ac83e
paulAAV98 <paavreas998@gmail.com>
2020-01-07 17:59:29 -0500

    ista Empleado

--- src/vista/Inicio.java
+++ src/vista/Inicio.java
@@ -103,8 +103,8 @@ public class Inicio extends javax.swing.JFrame {
 
         if(txtU.getText().equals(usuario) && pass.equals(passU)){
             JOptionPane.showMessageDialog(rootPane, "Ten un Buen dia");
-            //MainUsuario u = new MainUsuario();
-            //u.setVisible(true);
+             mainEmpleado e = new mainEmpleado();
+            e.setVisible(true);
             dispose();
         }else{
             if(txtU.getText().equals(admimisrador) && pass.equals(passA)){
