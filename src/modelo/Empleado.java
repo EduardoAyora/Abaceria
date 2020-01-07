8393bf4ea428c4e7cca9a18711ad1394f88e7824
EduardoAyora <eduardoaayora64@gmail.com>
2020-01-07 14:43:51 -0500

    modelo persona, empleado, proveedor

---
+++ src/modelo/Empleado.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author Eduardo Ayora
+ */
+public class Empleado extends Persona{
+    
+    private int tipoAdministrador;
+    private String usuario;
+    private String contrasenia;
+
+    public int getTipoAdministrador() {
+        return tipoAdministrador;
+    }
+
+    public void setTipoAdministrador(int tipoAdministrador) {
+        this.tipoAdministrador = tipoAdministrador;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    @Override
+    public String toString() {
+        return "Empleado{" + "tipoAdministrador=" + tipoAdministrador + ", usuario=" + usuario + ", contrasenia=" + contrasenia + '}';
+    }
+    
+    
+    
+}
---
+++ src/modelo/Persona.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author Eduardo Ayora
+ */
+public class Persona {
+    
+    private int id;
+    private String cedula;
+    private String nombre;
+    private String apellido;
+    private String direccion;
+    private String telefono;
+    private String celular;
+    private String activo;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getActivo() {
+        return activo;
+    }
+
+    public void setActivo(String activo) {
+        this.activo = activo;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "id=" + id + ", cedula=" + cedula + ", nombre=" + nombre + ", apellido=" + apellido + ", direccion=" + direccion + ", telefono=" + telefono + ", celular=" + celular + ", activo=" + activo + '}';
+    }
+    
+}
